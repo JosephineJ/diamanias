06c99d76d46d9061d5abc991d75277bb48df9087
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-05-30 09:43:12 +0200

    proper route change announcements

--- app/routes/chapters.js
+++ app/routes/chapters.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  title: 'Chapters of Diamanias - Overview',
   model() {
     // return this.get('store').findAll('chapter');
   }
--- app/routes/chapters/chapter.js
+++ app/routes/chapters/chapter.js
@@ -2,6 +2,13 @@ import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
 export default Route.extend({
+  titleToken: function(model) {
+    console.log({ model });
+    return `Number ${model.chapter.number}`;
+  },
+  title: function(token) {
+    return `Diamanias - Chapter ${token}`;
+  },
   async model(params) {
     let chapter = await this.get('store').find('chapter', params.id);
     return chapter.get('panels').then((panels) => {
