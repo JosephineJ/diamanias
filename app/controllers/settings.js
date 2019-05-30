9e162d88183e6a54a7b665a444a8f0c655c330c1
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-05-30 10:22:24 +0200

    cookie store anima settings

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,12 +1,18 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
-import { alias } from '@ember/object/computed';
+import { reads } from '@ember/object/computed';
 
 export default Controller.extend({
   settings: service(),
-  autoAnimationsEnabled: alias('settings.autoAnimationsEnabled'),
+  autoAnimationsEnabled: reads('settings.autoAnimationsEnabled'),
   currentAnimationSetting: computed('autoAnimationsEnabled', function() {
     return this.get('autoAnimationsEnabled') ? 'enabled' : 'off';
   }),
+  actions: {
+    toggleSettings(setting) {
+      let newValue = !this.get(setting);
+      this.settings.setSettings(setting, newValue);
+    },
+  }
 });
--- app/services/settings.js
+++ app/services/settings.js
@@ -1,5 +1,41 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+const { keys } = Object;
 
 export default Service.extend({
-  autoAnimationsEnabled: true,
+  cookies: service(),
+
+  autoAnimationsEnabled: null,
+
+  init() {
+    this._super(...arguments);
+    let autoplay = this.cookies.exists('autoAnimationsEnabled') ? this.cookies.read('autoAnimationsEnabled') : false;
+    console.log({ readValue: this.cookies.read('autoAnimationsEnabled') });
+    if (autoplay === 'no') {
+      this.set('autoAnimationsEnabled', false);
+    } else {
+      this.set('autoAnimationsEnabled', true);
+    }
+  },
+
+  setSettings(setting, value) {
+    console.log({ value });
+    this.set(setting, value);
+    let cookieValue = value ? 'yes' : 'no';
+    console.log({ cookieValue });
+    this.cookies.write('autoAnimationsEnabled', cookieValue);
+  },
+
+  allCookies: computed(function() {
+    let cookieService = this.get('cookies');
+    cookieService.write('now', new Date().getTime());
+
+    let cookies = cookieService.read();
+    return keys(cookies).reduce((acc, key) => {
+      let value = cookies[key];
+      acc.push({ name: key, value });
+
+      return acc;
+    }, []);
+  })
 });
